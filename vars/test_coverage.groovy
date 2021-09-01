2b53138f1daea705944de48aa99fef6a8152f82e
Julien Marrec <julien.marrec@gmail.com>
2021-09-01 11:27:01 +0200

    Add the start of a pipeline for testing purposes

---
+++ vars/test_coverage.groovy
@@ -0,0 +1,29 @@
+
+
+def call() {
+
+  node("desktop-ubuntu-1804") {
+    String base_dir = "/home/julien/Software/Others/Jenkins-Test/agent/TestCpp-GHA-Coverage";
+    String build_dir = "${base_dir}/build-coverage";
+
+    stage("Checkout") {
+
+      checkout([
+        $class: 'GitSCM',
+        branches:  [[name: "FETCH_HEAD"]],
+        userRemoteConfigs: [
+          [
+            credentialsId: 'jenkins-test-ssh-private-key-github',
+            refspec: "+refs/pull/${env.CHANGE_ID}/head:refs/remotes/origin/PR-${env.CHANGE_ID}",
+            url: 'git@github.com:jmarrec/TestCpp-GHA-Coverage.git']
+          ]
+      ])
+
+      if (fileExists(build_dir) == "false") {
+        sh("mkdir ${build_dir}") ;
+      }
+    } // end stage("checkout")
+
+
+  } // end node desktop-ubuntu-1804
+} // end call()
